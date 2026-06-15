8f62a1eb088f6de0a86312056a0e8d4fa8c96342
Sambhav Srivastava <sambhavsrivastava@Sambhavs-MacBook-Air.local>
2026-06-15 14:05:59 +0530

    Added parent portal validation

--- app4.py
+++ app4.py
@@ -35,6 +35,7 @@ class Student(db.Model):
 
     monthly_fee = db.Column(db.Integer, default=1000)
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -97,6 +98,123 @@ def students():
         "students.html",
         students=students
     )
+@app.route("/approve/<int:id>")
+def approve(id):
+
+    admission = Admission.query.get_or_404(id)
+
+    student_id = f"BSPS{id:03d}"
+
+    student = Student(
+        student_id=student_id,
+        student_name=admission.student_name,
+        father_name=admission.father_name,
+        phone=admission.phone,
+        student_class=admission.student_class,
+        monthly_fee=1000
+    )
+
+    db.session.add(student)
+
+    fee = Fee(
+        student_id=student_id,
+        student_name=admission.student_name,
+        month="July 2026",
+        amount=1000,
+        status="Pending"
+    )
+
+    db.session.add(fee)
+
+    db.session.commit()
+
+    return "Student Approved & Fee Record Created!"
+@app.route("/fees")
+def fees():
+
+    fees = Fee.query.all()
+
+    return render_template(
+        "fees.html",
+        fees=fees
+    )
+class Fee(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    student_id = db.Column(db.String(20))
+
+    student_name = db.Column(db.String(100))
+
+    month = db.Column(db.String(20))
+
+    amount = db.Column(db.Integer)
+
+    status = db.Column(db.String(20))
+
+@app.route("/payfee/<int:id>")
+def payfee(id):
+
+    fee = Fee.query.get_or_404(id)
+
+    fee.status = "Paid"
+
+    db.session.commit()
+
+    return "Fee Marked As Paid!"
+
+@app.route("/receipt/<int:id>")
+def receipt(id):
+
+    fee = Fee.query.get_or_404(id)
+
+    return render_template(
+        "receipt.html",
+        fee=fee
+    )
+
+@app.route("/parent", methods=["GET", "POST"])
+def parent():
+
+    if request.method == "POST":
+
+        student_id = request.form["student_id"]
+
+        student = Student.query.filter_by(
+            student_id=student_id
+        ).first()
+
+        if not student:
+            return "Student ID not found"
+
+        fees = Fee.query.filter_by(
+            student_id=student_id
+        ).all()
+
+        return render_template(
+            "parent_result.html",
+            student=student,
+            fees=fees
+        )
+
+    return render_template("parent.html")
+@app.route("/pay/<student_id>")
+def pay(student_id):
+
+    student = Student.query.filter_by(
+        student_id=student_id
+    ).first()
+
+    fees = Fee.query.filter_by(
+        student_id=student_id
+    ).all()
+
+    return render_template(
+        "pay.html",
+        student=student,
+        fees=fees
+    )
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
